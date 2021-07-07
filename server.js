e4d48d6e7df61da8f39736d89bf2ae412a9e1f02
SilentSwordAS <anuraag2002shukla@gmail.com>
2021-07-07 18:23:03 +0530

    json bay

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.use(express.json());
 
 app.use(cors());
 
+app.get('/', (req, res) => res.send('it is working'))
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 
